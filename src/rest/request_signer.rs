d328195eae9912fb21e568ede0bb1331185b09d2
Maxim Gorin <maksim.g@simple.app>
2023-10-31 22:29:54 +0300

    Add unit test

--- src/rest/request_signer.rs
+++ src/rest/request_signer.rs
@@ -49,4 +49,15 @@ mod tests {
         let source_sign = "e09cb7d69cef805a0f3092c770df60f2e1e91fb3ebdedc8f85f713a7369ba0e5";
         assert_eq!(source_sign, sign);
     }
+
+    #[test]
+    fn generate_sign_with_body() {
+        let key = "JHDOFKyrhlonPfoXrXiMeFqKRhWYdXTv".to_string();
+        let data = "{\"cryptoAmount\":\"0.00163967\",\"cryptoCurrency\":\"BTC\",\"customOrderId\":\"\",\"destinationWallet\":\"mjEcj2LA3vj1nDi8ZD3QMCs9kNqVk7Dpee\",\"fiatAmount\":\"50\",\"fiatCurrency\":\"USD\",\"status\":\"created\",\"tapOnFeeAmount\":\"\",\"tapOnFeeCurrency\":\"\",\"transactionHashes\":null,\"transactionId\":\"cbd38c1b-721a-4fa5-948d-8a628073084b\"}";
+
+        let sign = GateFiSigner::generate_sign(&key, &data);
+
+        let source_sign = "a3c0624fdf21d89e4d35614874b3ac0ef1ed723606c39ab761c75d86e7be0047";
+        assert_eq!(source_sign, sign);
+    }
 }
